834073b07d0fff2de1941d07694f9ebe658948e9
Kye <kyenelson3@gmail.com>
2022-06-01 20:48:04 -0700

    Chapter 4: Ray class and camera setup

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include "vec3.h"
+#include "ray.h"
 #include "color.h"
 
 #include <iostream>
 
+color ray_color(const Ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());  // normalize direction
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-t) * color(1.0, 1.0, 1.0) + t*color(1.0, 0.4, 1.0); //lerp
+}
+
 int main() 
 {
-    // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    // Image propterties
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0; // Distance from the eye to plane.
+
+    auto origin = point3(0, 0, 0); // eye location
+    auto horizontal = vec3(viewport_width, 0, 0); // offset vector
+    auto vertical = vec3(0, viewport_height, 0); // offset vector
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length); // lower left of plane
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -17,7 +36,10 @@ int main()
         std::cerr << "\rScanlines remaining: " << i << std::flush;
         for (int j = 0; j < image_width; j++)
         {
-            color pixel_color(double(j) / (image_width - 1), double(i) / (image_height - 1), 0.25);
+            auto u = double(j) / (image_width - 1);
+            auto v = double (i) / (image_height - 1);
+            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin); // cast rays across plane
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
---
+++ make.bat
@@ -0,0 +1,2 @@
+g++ -I./common/ -g -Wall -std=c++17 main.cpp common/*.cpp
+	a.exe > image.ppm
